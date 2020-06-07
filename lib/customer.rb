999dc8998f687b9a1941e8819357b8619888806b
Marcos Velasco <devslife7@gmail.com>
2020-06-07 15:13:21 -0400

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Customer
-  
+    attr_accessor :name, :age
+
+    @@all = []
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        meal = Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        # binding.pry
+        arr = []
+        Meal.all.each do |meal|
+            if meal.customer == self
+                arr << meal.waiter
+            end
+        end
+        # binding.pry
+        arr
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,37 @@
 class Waiter
+    attr_accessor :name, :experience
+
+    @@all = []
+    def initialize(name, experience)
+        @name  = name
+        @experience = experience
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def best_tipper
+        biggest_tip = 0
+        biggest_tipper = ""
+        Meal.all.each do |meal|
+            if meal.tip > biggest_tip
+                biggest_tip = meal.tip
+                biggest_tipper = meal.customer                
+            end
+        end
+        biggest_tipper
+    end
 
 end
