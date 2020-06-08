b1362792e2d4e2a52176dc92b0f65dcb6ef13293
steve <stevepesce879@gmail.com>
2020-06-07 20:39:15 -0400

    added working Waiter::most_frequent_customer, tested it with pry

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Waiter
  
     attr_accessor :name, :yrs_experience
@@ -32,8 +33,44 @@ class Waiter
                 max_tip_from = meal.customer
             end
         end
-
         return max_tip_from
+    end
+    #returns array of the most frequent customers
+    #if only one customer is the most frequent, returns the customer
+    def most_frequent_customer()
+        customer_map = Hash.new(0)
+        
+        #add each customer as a key, and their count as value
+        meals.each do |meal| 
+            if customer_map[meal.customer] != nil
+                customer_map[meal.customer] += 1
+            else
+                customer_map[meal.customer] = 1
+            end   
+        end     
+       
+        #amount of visits by customer
+        cust_count = 0
+        #most frequent (or the last of all most frequents)
+        best_cust = nil
+        #set new most visiting customer
+        customer_map.each do |(key,value)|
+            if value >= cust_count
+                cust_count = value
+                best_cust = key                
+            end            
+        end
+        #add all customers that match,
+        #more than one customer may have visited the same amount of times
+        best_customers = []
+        customer_map.each do |(key,value)|
+            if value == cust_count
+                best_customers << key
+            end
+        end
+        #return single customer  or return the array
+        return best_customers if best_customers.length > 1
+        return best_customers[0]
     end
    
 end
